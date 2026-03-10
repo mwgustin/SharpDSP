8911022b44179ad554ccbfed192ffc6d570f936a
Mike Gustin <gustin.mike@gmail.com>
2026-03-10 16:24:40 -0500

    pingpong delay

--- SharpDSP.Library/PingPongDelay.cs
+++ SharpDSP.Library/PingPongDelay.cs
@@ -1,32 +1,28 @@
 namespace SharpDSP.Library;
 
-
 public class PingPongDelay
 {
     private DelayLine leftDelay;
     private DelayLine rightDelay;
-    
-    // We only need one set of parameters since they usually 
-    // share the same timing for a rhythmic "bounce"
+
     private SmoothedParameter delayTime;
     private SmoothedParameter feedback;
     private SmoothedParameter mix;
 
-    // A secret "cross-talk" variable
-    private float lastLeftRead = 0f;
-    private float lastRightRead = 0f;
+    // The "Memory" of the previous sample's output for cross-feedback
+    private float lastLeftOutput = 0f;
+    private float lastRightOutput = 0f;
 
     public PingPongDelay(float maxDelaySeconds)
     {
-        int samples = (int)(maxDelaySeconds * Constants.SampleRate);
+        int samples = MathUtils.SecondsToSamples(maxDelaySeconds);
         leftDelay = new DelayLine(samples);
         rightDelay = new DelayLine(samples);
-        
-        // Initialize parameters...
-        delayTime = new SmoothedParameter(0.5f * Constants.SampleRate, 0.02f); // default 500ms delay
+
+        // Standard 20ms smoothing
+        delayTime = new SmoothedParameter(0.5f * Constants.SampleRate, 0.02f); 
         feedback = new SmoothedParameter(0.5f, 0.02f);
-        mix = new SmoothedParameter(0.5f, 0.02f); // default 50% wet/dry mix
-        
+        mix = new SmoothedParameter(0.5f, 0.02f);
     }
 
     public void Process(float[] leftBuffer, float[] rightBuffer)
@@ -37,27 +33,37 @@ public class PingPongDelay
             float curFeedback = feedback.Next();
             float curMix = mix.Next();
 
-            // 1. The Ping: Left Input + Right's Previous Feedback
-            // We "throw" the Right feedback into the Left line
-            float leftInput = leftBuffer[i] + (lastRightRead * curFeedback);
+            // 1. Sum Input to Mono (Standard for Ping-Pong)
+            float monoInput = (leftBuffer[i] + rightBuffer[i]) * 0.5f;
+
+            // 2. THE FIGURE-8 FEEDBACK LOGIC
+            // Left gets: Mono Input + (Previous Right Output * Feedback)
+            float inputL = monoInput + (lastRightOutput * curFeedback);
             
-            // 2. The Pong: Right Input + Left's Current Feedback
-            // We "throw" the Left feedback into the Right line
-            float rightInput = rightBuffer[i] + (lastLeftRead * curFeedback);
+            // Right gets: ONLY (Previous Left Output * Feedback)
+            // Note: We don't add monoInput here! If we did, both sides would 
+            // trigger at the same time.
+            float inputR = (lastLeftOutput * curFeedback);
 
-            // 3. Process the lines manually
-            // We use 0 feedback internally because we are 
-            // handling the cross-feedback logic here in this class
-            float wetL = leftDelay.ProcessSampleManual(leftInput, curDelay, 0, 1.0f);
-            float wetR = rightDelay.ProcessSampleManual(rightInput, curDelay, 0, 1.0f);
+            // 3. Process individual lines (Set internal feedback to 0)
+            // We set mix to 1.0 because we want the raw 'wet' signal to mix ourselves
+            float wetL = leftDelay.ProcessSampleManual(inputL, curDelay, 0, 1.0f);
+            float wetR = rightDelay.ProcessSampleManual(inputR, curDelay, 0, 1.0f);
 
-            // 4. Update our "cross-talk" memory
-            lastLeftRead = wetL;
-            lastRightRead = wetR;
+            // 4. Update memory for the next sample
+            lastLeftOutput = wetL;
+            lastRightOutput = wetR;
 
-            // 5. Final Mix
+            // 5. Final Output Mix
             leftBuffer[i] = (leftBuffer[i] * (1.0f - curMix)) + (wetL * curMix);
             rightBuffer[i] = (rightBuffer[i] * (1.0f - curMix)) + (wetR * curMix);
         }
+    }
+
+    public void UpdateParameters(float timeInSec, float feedbackAmt, float mixAmt)
+    {
+        delayTime.SetTarget(timeInSec * Constants.SampleRate);
+        feedback.SetTarget(feedbackAmt);
+        mix.SetTarget(mixAmt);
     }
 }
